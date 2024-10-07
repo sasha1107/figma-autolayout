5f6ced12f3a49f012b1755b660e522c9d68b6c57
sasha1107 <sasha1107@naver.com>
2024-10-07 23:58:39 +0900

    feat: radix theme, tailwind config

---
+++ src/components/DesignPanel.tsx
@@ -0,0 +1,11 @@
+import { Section } from "@radix-ui/themes";
+
+const DesignPanel = () => {
+  return (
+    <Section className="fixed h-dvh p-8 right-0">
+      <div className="bg-[#2c2c2c] rounded h-full">DesignPanel</div>
+    </Section>
+  );
+};
+
+export default DesignPanel;
