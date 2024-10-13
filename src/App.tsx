d2ed045636f0fe6708e5177885e923733083e893
sasha1107 <sasha1107@naver.com>
2024-10-13 22:47:46 +0900

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import DesignPanel from "./components/DesignPanel";
 
 function App() {
   return (
-    <div className="w-dvw h-dvh">
-      <DesignPanel />
-    </div>
+    <ThemeProvider>
+      <div className="w-dvw h-dvh">
+        <DesignPanel />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/DesignPanel.tsx
+++ src/components/DesignPanel.tsx
@@ -1,10 +1,23 @@
-import { Section } from "@radix-ui/themes";
-
 const DesignPanel = () => {
   return (
-    <Section className="fixed h-dvh p-8 right-0">
-      <div className="bg-[#2c2c2c] rounded h-full">DesignPanel</div>
-    </Section>
+    <section className="fixed h-dvh p-8 right-0 w-1/6">
+      <div className="border h-full rounded-md flex flex-col divide-y">
+        <div className="flex flex-col p-4">
+          <div>Auto layout</div>
+          <div className="grid grid-cols-7 grid-rows-5">
+            <div className="col-span-3">w</div>
+            <div className="col-span-3">h</div>
+            <div className="col-span-3 col-start-1">col .row</div>
+            <div className="col-span-3 row-span-2">3*3</div>
+            <div className="col-span-3 col-start-1">gap</div>
+            <div className="col-span-3 col-start-1">px</div>
+            <div className="col-span-3">py</div>
+            <div className="">individual padding</div>
+            <div className="col-span-7">clip content</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
