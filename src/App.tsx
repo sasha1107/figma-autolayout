8f0f8e00af381f375f11c0a9ab8b152bdf8d5b9c
sasha1107 <sasha1107@naver.com>
2024-10-15 00:29:50 +0900

    feat: toggle auto layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { twJoin, twMerge } from "tailwind-merge";
+import { useAtomValue } from "jotai";
+import { autoLayoutAtom } from "@/store/autoLayoutAtom";
 import { ThemeProvider } from "@/components/theme-provider";
 import DesignPanel from "./components/DesignPanel";
 import LayerPanel from "./components/LayerPanel";
 import Box from "./components/Box";
 
 function App() {
+  const autoLayout = useAtomValue(autoLayoutAtom);
   return (
     <ThemeProvider>
       <div className="w-dvw h-dvh flex justify-center items-center">
         <LayerPanel />
         <DesignPanel />
-        <div>
+        <div className={twMerge(twJoin(autoLayout && "flex"))}>
           <Box className="bg-slate-400">A</Box>
           <Box className="bg-slate-500">B</Box>
           <Box className="bg-slate-600">C</Box>
--- src/components/DesignPanel.tsx
+++ src/components/DesignPanel.tsx
@@ -1,20 +1,44 @@
+import { Toggle } from "@/components/ui/toggle";
+import { LayoutPanelLeft } from "lucide-react";
+import { useAtom } from "jotai";
+import { autoLayoutAtom } from "@/store/autoLayoutAtom";
+
 const DesignPanel = () => {
+  const [autoLayout, setAutoLayout] = useAtom(autoLayoutAtom);
   return (
     <section className="fixed h-dvh p-8 right-0 w-1/6">
       <div className="border h-full rounded-md flex flex-col divide-y">
         <div className="flex flex-col p-4">
-          <div>Auto layout</div>
-          <div className="grid grid-cols-7 grid-rows-5">
-            <div className="col-span-3">w</div>
-            <div className="col-span-3">h</div>
-            <div className="col-span-3 col-start-1">col .row</div>
-            <div className="col-span-3 row-span-2">3*3</div>
-            <div className="col-span-3 col-start-1">gap</div>
-            <div className="col-span-3 col-start-1">px</div>
-            <div className="col-span-3">py</div>
-            <div className="">individual padding</div>
-            <div className="col-span-7">clip content</div>
+          <div className="flex justify-between items-center">
+            <div>{autoLayout ? "Auto layout" : "Layout"}</div>
+            <Toggle
+              aria-label="Toggle bold"
+              pressed={autoLayout}
+              onPressedChange={(pressed) => setAutoLayout(pressed)}
+            >
+              <LayoutPanelLeft className="h-4 w-4" />
+            </Toggle>
           </div>
+          {autoLayout ? (
+            <div className="grid grid-cols-7 grid-rows-5">
+              <div className="col-span-3">w</div>
+              <div className="col-span-3">h</div>
+              <div className="col-span-3 col-start-1">col .row</div>
+              <div className="col-span-3 row-span-2">3*3</div>
+              <div className="col-span-3 col-start-1">gap</div>
+              <div className="col-span-3 col-start-1">px</div>
+              <div className="col-span-3">py</div>
+              <div className="">individual padding</div>
+              <div className="col-span-7">clip content</div>
+            </div>
+          ) : (
+            <div className="grid grid-cols-7 grid-rows-2">
+              <div className="col-span-3">w</div>
+              <div className="col-span-3">h</div>
+              <div>constraint</div>
+              <div className="col-span-7">clip content</div>
+            </div>
+          )}
         </div>
       </div>
     </section>
